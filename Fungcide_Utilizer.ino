e667d9d9103689aa704ee822bcb88bd010f38c19
MHeidar1 <62836413+MHeidar1@users.noreply.github.com>
2020-10-26 19:58:47 +0200

    Update Fungcide_Utilizer.ino

--- Fungcide_Utilizer.ino
+++ Fungcide_Utilizer.ino
@@ -1,19 +1,25 @@
 #include "dht.h"
 dht DHT;
-int dht_apin = A0;
-int sensorPin = A1; 
-int sensorValue;  
+int dht_pin = A0;
+int moisture_pin = A1; 
+int rain_pin = A2; 
+int moisture_value; 
+int rain_value;  
 
 void setup(){
  
   Serial.begin(9600);
   delay(500);//Delay to let system boot
-  Serial.println("Humidity    -    Temperature    -    Soil Moisture\n"); 
+  pinMode(dht_pin,INPUT);
+  pinMode(moisture_pin,INPUT);
+  pinMode(rain_pin,INPUT);
+  Serial.println("Humidity     -     Temperature     -     Soil Moisture     -     Rain\n"); 
 }
  
 void loop(){
-    DHT.read11(dht_apin);
-    sensorValue = analogRead(sensorPin); 
+    DHT.read11(dht_pin);
+    moisture_value = analogRead(moisture_pin); 
+    rain_value = analogRead(rain_pin); 
     Serial.print("Humidity = ");
     Serial.print(DHT.humidity);
     Serial.print("%  ");
@@ -21,8 +27,8 @@ void loop(){
     Serial.print(DHT.temperature); 
     Serial.print("C  ");
     Serial.print("Soil Moisture = ");
-    Serial.println(sensorValue);
-    delay(5000);
+    Serial.print(moisture_value);
+    Serial.print("  Raindrops = ");
+    Serial.println(rain_value);
+    delay(2000);
 }
-
-
